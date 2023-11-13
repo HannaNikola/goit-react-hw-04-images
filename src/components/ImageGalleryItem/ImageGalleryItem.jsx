91d43dc9a85e076c666a840ef092095bb6a1abfc
HannaNikola <h.nikolaienko32@gmail.com>
2023-11-13 19:50:17 +0100

    refactoring-searchBar-ImageGallaryItem

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,6 +1,6 @@
 
 import { GalleryIitem, Image } from "./ImageGalleryItem.styled";
-import { Component } from "react";
+import {useEffect, useState } from "react";
 import Modal from 'react-modal';
     
 const customStyles = {
@@ -18,41 +18,46 @@ const customStyles = {
 };
 Modal.setAppElement('#root');
 
-export class ImageGalleryItem extends Component {
 
-    state = {
-        isModalOpen: false, 
-    }
-    componentDidMount() {
-        window.addEventListener('keydown', this.hendalKeyDown )
-    }
 
-    componentWillUnmount() {
-        window.addEventListener('keydown', this.hendalKeyDown)
-    }
-    openModal = () => {
-        this.setState({ isModalOpen: true });
+export const ImageGalleryItem = ({ image }) => {
+    
+
+    const [isModalOpen, setIsModalOpen] = useState(false); 
+   
+
+    
+   const  openModal = () => {
+    setIsModalOpen(true);
     };
 
-    closeModal = () => {
-        this.setState({isModalOpen: false})
-    }
-    hendalKeyDown = (event) => {
-        if (event.code === 'Escape' && this.state.isModalOpen) {
-            this.closeModal();
-     }
- }
 
-    render() {
-        const { isModalOpen } = this.state
-        const { image } = this.props
-        
+    const closeModal = () => {
+     setIsModalOpen(false);
+    }
+   
+
+
+
+
+    useEffect(() => {
+        const hendalKeyDown = (event) => {
+            if (event.code === 'Escape' && isModalOpen) {
+                closeModal();
+            }
+        }
+        window.addEventListener('keydown', hendalKeyDown);
+        return () => {
+            window.addEventListener('keydown', hendalKeyDown);
+        }
+    }, [isModalOpen])
+  
         return (
             <GalleryIitem className="GalleryItem">
-                <Image src={image.webformatURL} alt="" onClick={this.openModal} />
+                <Image src={image.webformatURL} alt="" onClick={openModal} />
                 <Modal
                     isOpen={isModalOpen}
-                    onRequestClose={this.closeModal}
+                    onRequestClose={closeModal}
                     style={customStyles}
                     contentLabel="Large Image Modal"
                 >
@@ -61,7 +66,88 @@ export class ImageGalleryItem extends Component {
                 </Modal>
             </GalleryIitem>
         );
-    }
+  
 
 }
 
+
+
+
+
+
+// useEffect(() => {
+//     localStorage.setItem('contacts', JSON.stringify(contacts));
+// }, [contacts])
+
+// useEffect(() => {
+//     const savedContacts = localStorage.getItem('contacts');
+//     if (savedContacts !== null) {
+//         setContacts(JSON.parse(savedContacts));
+//     }
+// }, []);
+
+
+
+
+
+// const customStyles = {
+//     content: {
+//         top: '50%',
+//         left: '50%',
+//         right: 'auto',
+//         bottom: 'auto',
+//         marginRight: '-50%',
+//         transform: 'translate(-50%, -50%)',
+//     },
+//     overlay: {
+//         backgroundColor: 'rgba(0, 0, 0, 0.7)',
+//     },
+// };
+// Modal.setAppElement('#root');
+
+// export class ImageGalleryItem extends Component {
+
+//     state = {
+//         isModalOpen: false,
+//     }
+//     componentDidMount() {
+//         window.addEventListener('keydown', this.hendalKeyDown)
+//     }
+
+//     componentWillUnmount() {
+//         window.addEventListener('keydown', this.hendalKeyDown)
+//     }
+//     openModal = () => {
+//         this.setState({ isModalOpen: true });
+//     };
+
+//     closeModal = () => {
+//         this.setState({ isModalOpen: false })
+//     }
+//     hendalKeyDown = (event) => {
+//         if (event.code === 'Escape' && this.state.isModalOpen) {
+//             this.closeModal();
+//         }
+//     }
+
+//     render() {
+//         const { isModalOpen } = this.state
+//         const { image } = this.props
+
+//         return (
+//             <GalleryIitem className="GalleryItem">
+//                 <Image src={image.webformatURL} alt="" onClick={this.openModal} />
+//                 <Modal
+//                     isOpen={isModalOpen}
+//                     onRequestClose={this.closeModal}
+//                     style={customStyles}
+//                     contentLabel="Large Image Modal"
+//                 >
+//                     <Image src={image.webformatURL} alt="description" />
+
+//                 </Modal>
+//             </GalleryIitem>
+//         );
+//     }
+
+// }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,31 +1,30 @@
-import { Component } from "react";
+import {  useState } from "react";
 import { SearchbarStyle, Input, ButtonLabel, Button, Form } from "./Searchbar.styled";
 import { AiOutlineSearch } from "react-icons/ai";
 
-export class Searchbar extends Component {
-    state = {
-    query: '',
+export const Searchbar = ({ onSubmit }) => {
+    
+    const [query, setQuery] = useState('');
+    
 
-    }
-
-    handleSubmit = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(this.state.query)
-        this.props.onSubmit(this.state.query);
-        
-    }
-    handleChange = (event) => {
-        this.setState({ query: event.target.value });
+        onSubmit(query);
+    };
+
+
+    const handleChange = (event) => {
+        setQuery(event.target.value);
         
 };
     
    
 
 
-    render() {
+   
         return (
             <SearchbarStyle className="searchbar">
-                <Form className="form" onSubmit={this.handleSubmit}>
+                <Form className="form" onSubmit={handleSubmit}>
                     <Button type="submit" className="button">
                         <AiOutlineSearch />
                         <ButtonLabel className="button-label">Search</ButtonLabel>
@@ -37,14 +36,14 @@ export class Searchbar extends Component {
                         autoComplete="off"
                         autoFocus
                         placeholder="Search images and photos"
-                        value={this.state.query}
-                        onChange={this.handleChange}
+                        value={query}
+                        onChange={handleChange}
                         
                     />
                 </Form>
             </SearchbarStyle>
         );
-    }
+    
 }
 
 
